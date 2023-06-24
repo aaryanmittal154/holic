a5ea4a3b6f8b6128e3c6197c5573651e614908d9
Aaryan Mittal <aaryanmittal154@gmail.com>
2023-06-24 05:10:49 -0700

    Updated

---
+++ src/app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://holicai.tech/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://holicai.tech/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://holicai.tech/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://holicai.tech/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
